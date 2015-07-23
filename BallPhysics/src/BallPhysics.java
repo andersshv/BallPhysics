5de84e7ff797d14d200ffe1cf8d968c4066e4bea
andersshv@gmail.com <andersshv@gmail.com>
2015-07-23 20:48:29 +0200

    asdfasdf

--- BallPhysics/src/BallPhysics.java
+++ BallPhysics/src/BallPhysics.java
@@ -24,6 +24,7 @@ public class BallPhysics extends JFrame {
         
         System.out.println("adsfasdfasdf");
         System.out.println("adsfasdfasdf");
+        System.out.println("adsfasdfasdf");
 	}
 
 }
